5b9b192879abf5a04441e7731a0d154590ace074
andrey_mardash <andrey.mardash@gmail.com>
2021-05-02 07:26:59 +0300

    Rendering data from our data base and sending it to our template files in product_create_view() function

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,37 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+from .forms import ProductForm
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {
+#         "form": form
+#     }
+
+#     return render(request, 'products/product_create.html', context)
+
+def product_create_view(request):
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+        # Product.objects.create(title=my_new_title)
+    context = {}
+    return render(request, 'products/product_create.html', context)
+
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+    context = {
+        "object": obj
+    }
+
+    return render(request, "products/product_detail.html", context)
