a4491d72cd990d5c3ea3075d0b025bade02fe836
andrey_mardash <andrey.mardash@gmail.com>
2021-05-03 07:12:24 +0300

    Added render initial data function in src/products/views.py

--- src/products/views.py
+++ src/products/views.py
@@ -1,26 +1,45 @@
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
+
+# Create your views here
+
+def render_initial_data(request):
+    initail_data = {
+        'title': "This is my awesome title"
+    }
+    form = RawProductForm()
+    context = {
+        "form": form
+    }
+    return rende(request, "products/product_create.html", context)
 
 # def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
-
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
 #     context = {
-#         "form": form
+#         "form": my_form
 #     }
-
 #     return render(request, 'products/product_create.html', context)
 
 def product_create_view(request):
-    if request.method == "POST":
-        my_new_title = request.POST.get('title')
-        print(my_new_title)
-        # Product.objects.create(title=my_new_title)
-    context = {}
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        "form": form
+    }
+
     return render(request, 'products/product_create.html', context)
 
 # Create your views here.
