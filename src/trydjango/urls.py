9c20f59de8a7b7e0f977869b709efde9f14cf108
andrey_mardash <andrey.mardash@gmail.com>
2021-05-04 05:28:35 +0300

    Commented out most of the paths except for products/<int:id>/ and products/<int:id>/delete

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,14 +17,34 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, products_view, about_view,shoplist_view
-from products.views import product_detail_view, product_create_view
-
+# from products.views import product_detail_view, product_create_view, render_initial_data,
+from products.views import dynamic_lookup_view, product_delete_view
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('products/', products_view),
-    path('admin/', admin.site.urls),
-    path('about/', about_view),
-    path('shoplist/', shoplist_view),
-    path('product/', product_detail_view),
-    path('create/', product_create_view)
+    path('products/<int:id>/', dynamic_lookup_view),
+    path('products/<int:id>/delete/', product_delete_view),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # path('', home_view, name='home'),
+    # path('products/', products_view),
+    # path('admin/', admin.site.urls),
+    # path('about/', about_view),
+    # path('shoplist/', shoplist_view),
+    # path('product/', product_detail_view),
+    # path('create/', product_create_view)
+
 ]
