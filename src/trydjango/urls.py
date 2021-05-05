38a5589d41f2f24925cce10fd0eafead6296ba85
andrey_mardash <andrey.mardash@gmail.com>
2021-05-05 07:07:18 +0300

    Created dynamic urls to get or delete any product in db and also assigned names to eache path so in case I change the path in urls.py my views.py file would render the path in urls.py. It's been done so that I wouldn't need to do this all manually

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -18,10 +18,11 @@ from django.urls import path
 
 from pages.views import home_view, products_view, about_view,shoplist_view
 # from products.views import product_detail_view, product_create_view, render_initial_data,
-from products.views import dynamic_lookup_view, product_delete_view
+from products.views import dynamic_lookup_view, product_delete_view, product_list_view
 urlpatterns = [
-    path('products/<int:id>/', dynamic_lookup_view),
-    path('products/<int:id>/delete/', product_delete_view),
+    path('products/<int:id>/', dynamic_lookup_view, name="product-detail"),
+    path('products/<int:id>/delete/', product_delete_view, name="porduct-delete"),
+    path('products/', product_list_view, name="product_list")
 
 
 
