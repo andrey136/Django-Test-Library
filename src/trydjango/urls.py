a4b83be246a7d6619c8158c82fdbe735a656be5f
andrey_mardash <andrey.mardash@gmail.com>
2021-05-06 06:59:55 +0300

    urlpatterns = [path(products/, include(products.urls)), ...] --> I created a new file products/urls.py where I pasted all the urls for rendering my product objects, so I included that file to trydjando/urls.py

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -14,38 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from pages.views import home_view, products_view, about_view,shoplist_view
-# from products.views import product_detail_view, product_create_view, render_initial_data,
-from products.views import dynamic_lookup_view, product_delete_view, product_list_view
+from pages.views import home_view, about_view, shoplist_view
+
 urlpatterns = [
-    path('products/<int:id>/', dynamic_lookup_view, name="product-detail"),
-    path('products/<int:id>/delete/', product_delete_view, name="porduct-delete"),
-    path('products/', product_list_view, name="product_list")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # path('', home_view, name='home'),
-    # path('products/', products_view),
-    # path('admin/', admin.site.urls),
-    # path('about/', about_view),
-    # path('shoplist/', shoplist_view),
-    # path('product/', product_detail_view),
-    # path('create/', product_create_view)
-
+    path('products/', include('products.urls')),
+    path('', home_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('about/', about_view),
+    path('shoplist/', shoplist_view),
 ]
+
