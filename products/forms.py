3deea8e3d6941333315e1b215513bc3001361573
andrey_mardash <andrey.mardash@gmail.com>
2021-05-09 10:37:45 +0300

    Remake project's structure - Moved all the files from src to Django-Test-Library folder - Added .gitignore file from https://www.toptal.com/developers/gitignore/api/django - Removed migrations from .gitignore

---
+++ products/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+
+from .models import Product
+
+class ProductForm( forms.ModelForm):
+    title = forms.CharField(required=True, label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "class": "new-class name",
+                "id": "my-id-for-text-area",
+                "rows": 20,
+                "cols": 120
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
+    # email = forms.EmailField()
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'description',
+            'price'
+        ]
+
+    # def clean_title(self, *args, **kwargs):
+    #     title = self.cleaned_data.get("title")
+    #     if "CFE" not in title:
+    #         raise forms.ValidationError("This is not a valid title")
+    #     if "news" not in title:
+    #         raise forms.ValidationError("This is not a valid title")
+    #     return title
+
+    # def clean_email(self, *args, **kwargs):
+    #     email = self.cleaned_data.get("email")
+    #     if "edu" not in email:
+    #         raise forms.ValidationError("This is not a valid email")
+    #     return email
+
+class RawProductForm(forms.Form):
+    title       = forms.CharField(required=True, label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+                        required=False,
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class name",
+                                    "id": "my-id-for-text-area",
+                                    "rows": 20,
+                                    "cols": 120
+                                }
+                            )
+                        )
+    price       = forms.DecimalField(initial=199.99)
