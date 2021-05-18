c4a0283b6b1a9e7c77b679a55c70c6a681e01ac6
andrey_mardash <andrey.mardash@gmail.com>
2021-05-18 06:54:47 +0300

    Got rid of comas in models.py Product model. Turns out that was the reason why title and description fields havent been properly rendered in admin

--- src/products/models.py
+++ src/products/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.CharField(max_length=100),
-    description = models.TextField(blank=False),
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=False)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
