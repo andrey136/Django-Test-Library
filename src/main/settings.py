d20e7ba4ef0f8e1432145e4e1b77e9b341fe8f09
andrey_mardash <andrey.mardash@gmail.com>
2021-05-18 06:52:04 +0300

    added templates folder in settings.py

--- src/main/settings.py
+++ src/main/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', 'products/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
