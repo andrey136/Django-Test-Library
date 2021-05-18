d3ce48bf23eab1ca39bbf4274017617701fa2c7b
andrey_mardash <andrey.mardash@gmail.com>
2021-05-18 06:55:47 +0300

    Added new view functions like products_list_view and products_detail_view in products/views.py

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
+from products.forms import ProductForm
 
 # Create your views here.
 
 def home_view(request):
+    print('HOME VIEW')
     return render(request, 'home_view.html', {})
+
+def products_list_view(request):
+    objs = Product.objects.all()
+    context = {
+        "products": objs
+    }
+    return render(request, 'products/products_list.html', context)
+
+def products_detail_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        "product": obj
+    }
+    return render(request, 'products/products_detail.html', context)
+
+def products_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        "form": ProductForm
+    }
+    return render('create/', 'products/products_create.html', context)
